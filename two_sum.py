5aa1b848d4b05a93d2ae3b06c946118611927ff9
ahmdsabbir <ahmd.sabbir@gmail.com>
2023-04-10 16:00:52 +0600

    two sum

--- two_sum.py
+++ two_sum.py
@@ -1,16 +1,13 @@
 # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 def find_res(array, target):
-    result = []
-    for elem in array:
-        loc = array.index(elem)
-        for n_elem in array[loc+1:]:
-            if elem + n_elem == target:
-                return array.index(elem), array.index(n_elem)
-                result.append(array.index(elem), array.index(n_elem))
+    for i in range(len(array)):
+        for j in range(i+1, len(array)):
+            if array[i] + array[j] == target:
+                print(i, '&', j)
 
 if __name__=='__main__':
-    array = [4,5,8,99,6,7,9,90]
-    target = 94
+    array = [4,5,8,99,6,7,9,8, 90]
+    target = 16
 
-    print(find_res(array, target))
+    find_res(array, target)
